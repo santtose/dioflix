fdddd3876ba3b307a42e9b1036e2b913dbe8a08c
diogo_santos <diogo_santos@econeteditora.com.br>
2020-11-24 14:28:37 -0300

    Navegação, carousel

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/logoDioFlix.png';
 import Button from '../Button';
 
@@ -11,7 +12,7 @@ function Menu() {
                 <img className="Logo" src={Logo} alt="Dioflix"/>
             </a>
 
-            <Button as="a" className="ButtonLink" href="/">
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
                 Novo Video
             </Button>
         </nav>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Menu from './components/Menu';
-import BannerMain from './components/BannerMain';
-import Carousel from './components/Carousel';
-import dadosIniciais from './data/dados_iniciais.json';
-import Footer from './components/Footer';
+import Menu from '../../components/Menu';
+import BannerMain from '../../components/BannerMain';
+import Carousel from '../../components/Carousel';
+import dadosIniciais from '../../data/dados_iniciais.json';
+import Footer from '../../components/Footer';
 
-function App() {
+function Home() {
   return (
     <div style={{ background: '#141414' }}>
        <Menu/>
@@ -37,13 +37,9 @@ function App() {
           category={dadosIniciais.categorias[4]}
         />
 
-        <Carousel
-          category={dadosIniciais.categorias[5]}
-        />
-
         <Footer/>
     </div>
   );
 }
 
-export default App;
+export default Home;
